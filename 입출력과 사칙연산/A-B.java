07d89908bfc6a048051543d18937bf3ba4ee4954
DeveloperKGH <developerkgh@gmail.com>
2021-06-28 11:04:54 +0900

    A - B
    A - B

---
+++ 입출력과 사칙연산/A-B.java
@@ -0,0 +1,37 @@
+/* 
+A-B
+
+문제
+두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A-B를 출력한다.
+
+예제 입력 1 
+3 2
+
+예제 출력 1 
+1
+*/
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        calcToPrint(a, b);
+    };
+
+    public static void calcToPrint(int a, int b) {
+        System.out.println(a - b);
+    }
+
+}
